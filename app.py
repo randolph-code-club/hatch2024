e2bdb13629efd1891158d3616d72902f9be3ff0a
Greyden <vadercat1@icloud.com>
2024-03-02 17:18:32 -0600

    fixed up results

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, make_response, redirect
 import requests
 from flask import Flask
 import os
+import random
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -11,7 +12,18 @@ def index():
 
 @app.route("/results", methods = ['POST'])
 def results():
-    return render_template("results.html")
+    sequence = ''.join(random.choice('CTAG') for _ in range(20))
+    sequence2 = ""
+    for nucleotide in sequence:
+        if nucleotide == "A":
+            sequence2 += "T"
+        elif nucleotide == "T":
+            sequence2 += "A"
+        elif nucleotide == "C":
+            sequence2 += "G"
+        elif nucleotide == "G":
+            sequence2 += "C"
+    return render_template("results.html", sequence=sequence, sequence2=sequence2)
 
 if __name__ == "__main__":
     app.run()
